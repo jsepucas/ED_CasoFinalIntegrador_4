b66063919f6f525053b38f846a9a2f6bada2b4f9
jsepucas <jsepucas@myuax.com>
2024-04-07 19:23:17 +0200

    commitss

--- src/main/java/D_InterfazGraficaAvanzada/VentanaDeTexto.java
+++ src/main/java/D_InterfazGraficaAvanzada/VentanaDeTexto.java
@@ -5,3 +5,14 @@ import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+public class ValidadorEmailGUI extends JFrame {
+    private JTextField campoEmail;
+    private JLabel etiquetaValidacion;
+
+    public ValidadorEmailGUI() {
+        setTitle("Validador de Email");
+        setSize(300, 100);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        configurarUI();
+    }
+
