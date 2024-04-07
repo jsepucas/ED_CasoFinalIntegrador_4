4826a9ff876a35ab00a5a1ec3ef8a5cde36c7445
jsepucas <jsepucas@myuax.com>
2024-04-07 16:55:48 +0200

    Commitss

---
+++ src/main/java/C_BusquedaYGestion/AgendaDeContactos.java
@@ -0,0 +1,11 @@
+package C_BusquedaYGestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgendaDeContactos {
+    private List<Contacto> contactos;
+
+    public AgendaDeContactos() {
+        this.contactos = new ArrayList<>();
+    }
