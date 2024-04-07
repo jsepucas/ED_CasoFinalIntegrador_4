88c14d24eccb73e8f85b1ffc292b952df1869e94
jsepucas <jsepucas@myuax.com>
2024-04-07 18:24:48 +0200

    commitss

--- src/main/java/B_ComparadorYContador/AnalizadorTextoGUI.java
+++ src/main/java/B_ComparadorYContador/AnalizadorTextoGUI.java
@@ -6,3 +6,16 @@ import java.awt.event.ActionEvent;
 import java.io.File;
 import java.nio.file.Files;
 import java.util.Map;
+
+public class AnalizadorTextoGUI extends JFrame {
+    private JTextArea textArea;
+    private JButton analyzeButton, loadButton;
+    private final JFileChooser fileChooser;
+
+    public AnalizadorTextoGUI() {
+        setTitle("Analizador de Texto");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        fileChooser = new JFileChooser();
+        initUI();
+    }
