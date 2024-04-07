4a79600fd548cb440d26b0a98aeda5fd7f559d0b
jsepucas <jsepucas@myuax.com>
2024-04-07 18:18:44 +0200

    agh

--- src/main/java/B_ComparadorYContador/ComparadorDeArchivos.java
+++ src/main/java/B_ComparadorYContador/ComparadorDeArchivos.java
@@ -7,3 +7,16 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+
+public class ComparadorDeArchivos extends JFrame {
+    private JButton compareButton;
+    private JTextArea resultArea;
+    private final JFileChooser fileChooser;
+
+    public ComparadorDeArchivos() {
+        setTitle("Comparador de Archivos");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        fileChooser = new JFileChooser();
+        initUI();
+    }
