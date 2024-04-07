90abc4cfb81cdf7f87b76ac0d272ec3c23ab44d3
jsepucas <jsepucas@myuax.com>
2024-04-07 16:05:07 +0200

    Commitss

--- src/main/java/A_EditorDeTextoInteractivo/EditorDeTextoGUI.java
+++ src/main/java/A_EditorDeTextoInteractivo/EditorDeTextoGUI.java
@@ -6,5 +6,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 
-public class EditorDeTextoGUI {
+public class EditorDeTextoGUI extends JFrame {
+
+    private JTextArea textArea;
+    private JFileChooser fileChooser;
+
+    public EditorDeTextoGUI() {
+        super("Editor de Texto Interactivo");
+
+        textArea = new JTextArea();
+        fileChooser = new JFileChooser();
+
+
 }
