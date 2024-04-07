4ec61721b81afcfab4cea8b2605c0fa03baecb80
jsepucas <jsepucas@myuax.com>
2024-04-07 16:39:53 +0200

    Commitss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,3 +14,19 @@ public class Main {
         ComparadorDeArchivos comparador = new ComparadorDeArchivos();
         AnalizadorDeTexto analizador = new AnalizadorDeTexto();
 
+        // Ejemplo
+        try {
+            File file1 = new File("ruta/al/archivo1.txt");
+            File file2 = new File("ruta/al/archivo2.txt");
+
+            boolean sonIguales = comparador.comparar(file1, file2);
+            System.out.println("Los archivos son iguales: " + sonIguales);
+
+            long numPalabras = analizador.contarPalabras(file1);
+            System.out.println("Número de palabras en el archivo1: " + numPalabras);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
