5d777085264258e4957e2ba523cfdd860fd9c6ff
jsepucas <jsepucas@myuax.com>
2024-04-07 19:11:01 +0200

    commitss

--- src/main/java/C_BusquedaYGestion/BuscadorDePalabrasGUI.java
+++ src/main/java/C_BusquedaYGestion/BuscadorDePalabrasGUI.java
@@ -21,3 +21,40 @@ public class BuscadorDePalabrasGUI extends JFrame {
         selectorDeArchivos = new JFileChooser();
         iniciarUI();
     }
+
+    private void iniciarUI() {
+        areaDeTexto = new JTextArea();
+        areaDeTexto.setEditable(false);
+        campoDeBusqueda = new JTextField(20);
+        botonCargar = new JButton("Cargar Archivo");
+        botonBuscar = new JButton("Buscar Palabra");
+
+        botonCargar.addActionListener(this::accionCargar);
+        botonBuscar.addActionListener(this::accionBuscar);
+
+        JPanel panelNorte = new JPanel();
+        panelNorte.add(botonCargar);
+        panelNorte.add(new JLabel("Palabra:"));
+        panelNorte.add(campoDeBusqueda);
+        panelNorte.add(botonBuscar);
+
+        setLayout(new BorderLayout());
+        add(panelNorte, BorderLayout.NORTH);
+        add(new JScrollPane(areaDeTexto), BorderLayout.CENTER);
+
+        setVisible(true);
+    }
+
+    private void accionCargar(ActionEvent evento) {
+        if (selectorDeArchivos.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+            File archivo = selectorDeArchivos.getSelectedFile();
+            try {
+                String contenido = Files.readString(archivo.toPath());
+                areaDeTexto.setText(contenido);
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(this, "Error al cargar el archivo: " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+
+    
