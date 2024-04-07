88603a94c22a0368fc1ed17c15ff80edd7917fe1
jsepucas <jsepucas@myuax.com>
2024-04-07 16:57:46 +0200

    Commitss

--- src/main/java/C_BusquedaYGestion/AgendaDeContactos.java
+++ src/main/java/C_BusquedaYGestion/AgendaDeContactos.java
@@ -9,3 +9,15 @@ public class AgendaDeContactos {
     public AgendaDeContactos() {
         this.contactos = new ArrayList<>();
     }
+
+
+    public void agregarContacto(Contacto contacto) {
+        contactos.add(contacto);
+    }
+
+    public void eliminarContacto(Contacto contacto) {
+        contactos.remove(contacto);
+    }
+
+
+
