fe1b3a6ef8e2476c2fc7411e34458e6101b52c9e
jsepucas <jsepucas@myuax.com>
2024-04-07 17:06:26 +0200

    Commitss

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,23 +13,8 @@ public class Main {
         // GUI
         new EditorDeTextoGUI();
 
-        // Crear instancias de ComparadorDeArchivos y AnalizadorDeTexto
+        // instancias de ComparadorDeArchivos, AnalizadorDeTexto, BuscadorDePalabras yy AgendaDeContactos
         ComparadorDeArchivos comparador = new ComparadorDeArchivos();
         AnalizadorDeTexto analizador = new AnalizadorDeTexto();
-
-        // Ejemplo
-        try {
-            File file1 = new File("ruta/al/archivo1.txt");
-            File file2 = new File("ruta/al/archivo2.txt");
-
-            boolean sonIguales = comparador.comparar(file1, file2);
-            System.out.println("Los archivos son iguales: " + sonIguales);
-
-            long numPalabras = analizador.contarPalabras(file1);
-            System.out.println("Número de palabras en el archivo1: " + numPalabras);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
-
+        BuscadorDePalabras buscador = new BuscadorDePalabras();
+        AgendaDeContactos agenda = new AgendaDeContactos();
