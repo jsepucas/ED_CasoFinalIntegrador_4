abf9cf0adfaf29f6a25c88996849ab0349880bbd
jsepucas <jsepucas@myuax.com>
2024-04-07 19:26:13 +0200

    commitss

--- src/main/java/E_Validacion/HerramientaDibujoGUI.java
+++ src/main/java/E_Validacion/HerramientaDibujoGUI.java
@@ -5,3 +5,13 @@ import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
+
+public class HerramientaDibujoGUI extends JFrame {
+    private JPanel lienzo;
+
+    public HerramientaDibujoGUI() {
+        setTitle("Herramienta de Dibujo");
+        setSize(400, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        configurarUI();
+    }
